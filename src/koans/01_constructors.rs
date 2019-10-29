18e7a06c34ed8a012645d025832bfaa8e1696eb1
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-10-29 23:33:39 +0000

    First constructor test.

--- src/koans/01_constructors.rs
+++ src/koans/01_constructors.rs
@@ -1,12 +1,32 @@
+/// First things first: what is `ndarray` about?
+///
+/// Rust's standard library provides you with `Vec<T>`: a 1-dimensional array of elements
+/// of type `T`.
+///
+/// Sometimes one dimension is not enough.
+/// What if you want to do some linear algebra and a bunch of matrix computations?
+/// What if you want to play with tensors and deep learning algorithm?
+///
+/// You need an n-dimensional array: here comes `ndarray`!
+///
+/// It provides you with `Array`, a generalisation of `Vec<T>` to handle multiple dimensions.
+/// At the end of the workshop, `Array` (and `ArrayBase`) will be your new best friend.
+///
+/// But introductions first: how do you get your hands on one of these n-dimensional arrays?
 #[cfg(test)]
 mod constructors {
-    #[test]
-    fn casual_one() {
-        assert!(true);
-    }
+    use ndarray::Array;
 
     #[test]
-    fn constructors_normal() {
-        assert!(true);
+    // Given that `Array` is a generalisation of `Vec`,
+    // it's fair to expect that you can get a `Vec` and turn it into an `Array`.
+    fn from_vec() {
+        let vector: Vec<u32> = __;
+
+        let ndarray_vector = Array::from(vector);
+
+        assert_eq!(ndarray_vector.len(), 4);
+        assert_eq!(ndarray_vector[0], 1);
+        assert_eq!(ndarray_vector[2], 7);
     }
 }
