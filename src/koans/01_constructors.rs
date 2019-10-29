e16587d447eb0c1daacdad881a29958bd1e9a593
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-10-29 23:55:22 +0000

    One more comment

--- src/koans/01_constructors.rs
+++ src/koans/01_constructors.rs
@@ -36,7 +36,7 @@ mod constructors {
     // You are not forced to pass through a `Vec` to create an `Array`.
     //
     // The `array!` macro follows exactly the same syntax of the `vec!` macro
-    // for 1-dimensional arrays.
+    // for 1-dimensional arrays and gives you directly an `Array` instance.
     fn macro_literal() {
         let from_vector = Array::from(vec![0, 1, 2]);
         let with_macro = array![__];
