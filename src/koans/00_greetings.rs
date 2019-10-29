4e2c4602a13be5f556fb754f486209a57a3c561c
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-10-29 23:09:11 +0000

    Write workshop introduction

--- src/koans/00_greetings.rs
+++ src/koans/00_greetings.rs
@@ -1,7 +1,44 @@
+/// Happy RustFest!
+///
+/// It's my pleasure to welcome you to the "ML introduction to ndarray" workshop!
+///
+/// The material is structured as a series of exercises, or koans.
+///
+/// A koan is a riddle or puzzle that Zen Buddhists use during meditation to help them
+/// unravel greater truths about the world and about themselves.
+///
+/// Will you get the chance to unveil deeper insights about yourself during this session?
+/// Maybe, maybe not.
+/// But I'll try my best to take you from "what is this ndarray thing?"
+/// to "Look, ma! I can do this machine learning thing with it!".
+///
+/// If everything goes well, at the end of the session you will:
+/// - have implemented from scratch the K-means clustering algorithm;
+/// - know enough about `ndarray` and its ecosystem to go on and have fun with it!
+///
+/// **Practicalities**:
+/// - each koan is a file in the `koans` folder;
+/// - each file contains a test module with several tests inside it;
+/// - you can move along your journey with `cargo run`:
+///     - if you have filled in correctly all tests in a koan (or you just started)
+///       the console output will tell the name of the next koan you should get started with;
+///     - if something is wrong with your test cases, the console output will contain
+///       the compiler errors or test failures that you should investigate.
+///
+/// Enjoy!
 #[cfg(test)]
 mod greetings {
     #[test]
+    /// This is your starting block!
+    ///
+    /// In each test, you are expected to replace __ in order to make test pass.
+    /// Sometimes a one-liner (or a literal value) will be enough.
+    /// Sometimes you will have to write a bit more to get the job done.
+    ///
+    /// If you get stuck, don't hesitate to ping me!
     fn the_beginning_of_your_journey() {
-        assert!(__);
+        let i_am_ready_to_start = __;
+
+        assert!(i_am_ready_to_start);
     }
 }
