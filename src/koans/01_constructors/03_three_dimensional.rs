dc5ff580585ee488826509e59e586896b0c65fc0
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-11-01 22:30:35 +0000

    Add three dimensional

---
+++ src/koans/01_constructors/03_three_dimensional.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod constructors_three_dimensional {
+    use ndarray::{Array, array};
+
+    #[test]
+    fn three_dimensional() {
+        // Can you guess how the `array!` macro generalises to 3-dimensional arrays?
+        let tensor = array![
+            __
+        ];
+
+        assert_eq!(tensor.ndim(), 3);
+        // `dim()` returns the shape of an n-dimensional array as a tuple
+        assert_eq!(tensor.dim(), (3, 2, 2));
+    }
+}
