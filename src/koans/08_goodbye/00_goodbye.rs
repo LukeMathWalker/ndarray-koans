315979f2fa241c6436a84124d6695b6258950858
Luca Palmieri <lpalmieri@truelayer.com>
2019-11-08 22:47:13 +0000

    Port to solutions branch the new changes from master

---
+++ src/koans/08_goodbye/00_goodbye.rs
@@ -0,0 +1,50 @@
+/// Even the best journeys eventually come to an end.
+///
+/// I hope you found the workshop enjoyable and interesting.
+/// As it happens for all types of learning material and teaching mediums,
+/// it's difficult to assess if the ground covered was enough, the right amount
+/// or too much.
+///
+/// But I'd like you to approach this workshop as a starting point to go
+/// and venture off on your own projects and musings in the ML corner of the Rust ecosystem.
+/// Using `ndarray`, if that suits your purposes.
+///
+/// If you still have some time left before the end of the session (congrats, you
+/// were fast!) and you are wondering "What should I do next?", I have a
+/// bunch of proposals that might pick your interest:
+/// - if you have a background in Python, try to wrap the K-means algorithm you just
+///   implemented in a Python package to make it easier to consume for a larger
+///   ecosystem of users.
+///   https://github.com/rust-numpy/rust-numpy#write-a-python-module-in-rust is a good
+///   starting point;
+/// - if you are curious to find our how you could squeeze the last drop of performance
+///   out of `ndarray`, why don't you try to profile your K-means implementation with
+///   something like `flamegraph` ( https://github.com/ferrous-systems/flamegraph )?
+///   How much faster can you make it?
+/// - you did it once, why don't you do it again? Choose another ML algorithm
+///   and implement it in Rust with `ndarray`!
+///   Check out https://github.com/ddbourgin/numpy-ml/tree/master/numpy_ml
+///   if you need some inspiration/a from-scratch implementation to refer to using NumPy!
+///
+/// I am sure there are many other venues you can take it from here and
+/// I am curious to see them unfold, good luck!
+///
+/// I tried my best to do a good job preparing this workshop, but it is indeed my first
+/// time and I am sure that there is tremendous room for improvement.
+/// If you are willing to share some feedback on the workshop, please reach out to me
+/// either in person or asynchronously using:
+/// - GitHub (@LukeMathWalker)
+/// - Twitter (@algo_luca)
+/// - Email (rust@lpalmieri.com)
+///
+/// ~ It has been a pleasure to have you on board! ~
+///
+#[cfg(test)]
+mod goodbye {
+    #[test]
+    fn the_end_of_your_journey() {
+        let i_made_it = __;
+
+        assert!(i_made_it);
+    }
+}
